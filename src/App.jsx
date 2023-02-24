9c7f96634aa529b28e5caef3a60b31594f655649
Essien-SM <stephenyessien@gmail.com>
2023-02-24 00:07:57 +0000

    creating all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Experience from './components/experiences/Experience'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import Portfolio from './components/portfolio/Portfolio'
+import Service from './components/services/Service'
+import Testmonials from './components/testimonials/Testmonials'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+        <Header/>
+        <Nav/>
+        <About/>
+        <Experience/>
+        <Service/>
+        <Portfolio/>
+        <Testmonials/>
+        <Contact/>
+        <Footer/>
+    </>
   )
 }
 
