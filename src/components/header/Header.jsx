adf72e8b6482a6c2a9c18d926b53f1cfa30198a4
Essien-SM <stephenyessien@gmail.com>
2023-02-27 06:32:42 +0000

    importing headersocial

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -11,12 +12,15 @@ const Header = () => {
         <h1>Essien</h1>
         <h5 className='text-light'>Full Stack Developer</h5>
         <CTA/>
+        <HeaderSocials/>
 
         <div className="me">
           <img src={ME} alt="me" />
         </div>
 
         <a href="#contact" className='scroll__down'>Scroll Down</a>
+        
+       
       </div>
     </header>
   )
